ba946bef0270f00d27ea94dc3620a311eb7f2b50
Komens <1126972456@qq.com>
2020-10-27 14:06:15 +0800

    feat:完成基本UI制作

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <BrowserRouter>
-          <Switch>
-            <Route path="/" component={Home} />
-          </Switch>
-        </BrowserRouter>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,52 @@
-import React from 'react'
+import React from "react";
+import "../../style/home/index.scss";
+import imageURL from "../../assets/images/del_btn.svg";
+
+const TodoList = () => {
+  return (
+    <ul className="list">
+      <li className="list_item">
+        <div className="event_head">
+          <div className="content">这是内容区域</div>
+          <img className="btn_del" src={imageURL} alt="" />
+        </div>
+        <div className="event_footer">
+          <div className="done_time">完成时间：2020/10/10</div>
+          <div className="created_time">创建时间：2018/09/09</div>
+        </div>
+      </li>
+      <li className="list_item done">
+        <div className="event_head">
+          <div className="content">这是内容区域这是内容区域这是内容区域这是内容区域这是内容区域这是内容区域</div>
+          <img className="btn_del" src={imageURL} alt="" />
+        </div>
+        <div className="event_footer">
+          <div className="done_time">完成时间：2020/10/10</div>
+          <div className="created_time">创建时间：2018/09/09</div>
+        </div>
+      </li>
+    </ul>
+  );
+};
 
 export default () => {
-    return <div>HOME</div>
-}
+  return (
+    <div className="home">
+      <header className="home_header">
+        <h1>TODO</h1>
+        <div className="input">
+          <input type="text" className="input_text" />
+          <div className="input_btn">添加</div>
+        </div>
+      </header>
+      <div className="container">
+        <nav className="nav">
+          <div className="nav_item">全部</div>
+          <div className="nav_item">未完成</div>
+          <div className="nav_item nav_item-active">已完成</div>
+        </nav>
+        <TodoList />
+      </div>
+    </div>
+  );
+};
